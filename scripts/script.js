2f7e6d6316bcbe7d84791397e50c8d0876f20957
germaen <nicog425@newschool.edu>
2024-10-28 23:26:37 -0400

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,51 @@
-// API respondent Code, to test out random values please look at the commented out code below, strts at line 58
-
 const purpleGroup = document.getElementById('purple-grouped');
 
-async function getData(){
-    try{
+async function getData() {
+    try {
         const myURL = "http://api.weatherapi.com/v1/current.json?key=1aa1d13a384f4e0b994234559241410&q=New York&aqi=no";
-
         const response = await fetch(myURL);
         const data = await response.json();
 
-        const currentTime = data.location.localtime;
-        console.log(currentTime);
-
-        const timeHour = new Date(currentTime).getHours();
-        const tempFahr = data.current.temp_f;
-        console.log(tempFahr);
+        // Input Variables Here to Test Dummy Data, if want to test real time API please comment below out
+        const timeHour = 1; 
+        const tempFahr = 75; 
 
         thumpFx(tempFahr, timeHour);
 
-    } catch(error) {
+    } catch (error) {
         console.error(error.message);
     }
 
     function thumpFx(temp, hour) {
-        const nightTime = (hour >= 20 || hour < 6);
+        const nightTime = (hour >= 0 || hour < 2);  
         const thumpingCondition = (temp >= 40 && temp <= 90 && nightTime);
-    
+
         if (thumpingCondition) {
             let animationSpeed;
+            let gradientId;
 
             if (hour >= 20 && hour < 24) {
-                animationSpeed = 1.8; 
+                animationSpeed = 1.3; 
+                gradientId = "neonGradient2";
             } else if (hour >= 0 && hour < 2) {
-                animationSpeed = 0.001; 
+                animationSpeed = 0.125; 
+                gradientId = "neonGradient";
             } else if (hour >= 2 && hour < 6) {
-                animationSpeed = 2;
+                animationSpeed = 3;
+                gradientId = "neonGradient3";
             }
             
             purpleGroup.classList.add("thumping");
-
             purpleGroup.style.animationDuration = `${animationSpeed}s`;
+            purpleGroup.style.fill = `url(#${gradientId})`;
 
-            purpleGroup.style.fill = "url(#neonGradient)"; 
+        } else {
+            purpleGroup.classList.remove("thumping");
+            purpleGroup.style.animation = "none";
+            purpleGroup.style.fill = "#800080"; 
+        } 
+    }
+}
 
-    } else {
-        purpleGroup.classList.remove("thumping");
-        purpleGroup.style.animation = "none";
-        purpleGroup.style.fill = "#800080";
-    } 
-}
-}
 getData();
-setInterval(getData, 60 * 60 * 1000);
 
-// Test variable Code without realtime API
-
-// const purpleGroup = document.getElementById('purple-grouped');
-
-// async function getData(){
-//     try{
-//         const myURL = "http://api.weatherapi.com/v1/current.json?key=1aa1d13a384f4e0b994234559241410&q=New York&aqi=no";
-
-//         const response = await fetch(myURL);
-//         const data = await response.json();
-
-//         const timeHour = 21; 
-//         const tempFahr = 75;
-
-//         thumpFx(tempFahr, timeHour);
-
-//     } catch(error){
-//         console.error(error.message);
-//     }
-
-//     function thumpFx(temp, hour) {
-//         const nightTime = (hour >= 6 && hour < 18);
-//         const thumpingCondition = (temp >= 40 && temp <= 90 && nightTime);
-    
-//         if (thumpingCondition){
-//             let animationSpeed;
-
-//             if (hour >= 20 && hour < 24) {
-//                 animationSpeed = 1.8; 
-//             } else if (hour >= 0 && hour < 2) {
-//                 animationSpeed = 0.001; 
-//             } else if (hour >= 2 && hour < 6) {
-//                 animationSpeed = 2;
-//             }
-            
-//             purpleGroup.classList.add("thumping");
-//             purpleGroup.style.animationDuration = `${animationSpeed}s`;
-//             purpleGroup.style.fill = "url(#neonGradient)";
-
-//         } else{
-//             purpleGroup.classList.remove("thumping");
-//             purpleGroup.style.animation = "none";
-//             purpleGroup.style.fill = "#800080"; 
-//         } 
-//     }
-// }
-
-// getData();
